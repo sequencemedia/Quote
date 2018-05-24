d21d359cf128e8cefe864b9bcbe5fe8726329c8b
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 16:46:59 +0100

    `export`

--- travel/single/step3/index.js
+++ travel/single/step3/index.js
@@ -2,3 +2,10 @@ export step3 from './step3.js';
 export applyCampaignCode from './step3-apply-campaign-code.js';
 export applyBupaMember from './step3-apply-bupa-member.js';
 export applyBrokerCode from './step3-apply-broker-code.js';
+
+export {
+  step3,
+  applyCampaignCode,
+  applyBupaMember,
+  applyBrokerCode
+};
