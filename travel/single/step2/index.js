8fdb03fd9a5a93e2b82d53204ccae5f9a44d1ebd
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 18:31:41 +0100

    `export`/`import`

--- travel/single/step2/index.js
+++ travel/single/step2/index.js
@@ -3,7 +3,7 @@ import selectBasic from './step2-select-basic.js';
 import selectBasicNonMedical from './step2-select-basic-non-medical.js';
 import selectBasicNonMedicalTripCancellation from './step2-select-basic-non-medical-trip-cancellation.js';
 
-export {
+export default {
   step2,
   selectBasic,
   selectBasicNonMedical,
--- travel/single/step3/index.js
+++ travel/single/step3/index.js
@@ -3,7 +3,7 @@ import applyCampaignCode from './step3-apply-campaign-code.js';
 import applyBupaMember from './step3-apply-bupa-member.js';
 import applyBrokerCode from './step3-apply-broker-code.js';
 
-export {
+export default {
   step3,
   applyCampaignCode,
   applyBupaMember,
