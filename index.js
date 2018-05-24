965fc022b2d765fca67c07a17e69f4a8e0021074
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 12:58:23 +0100

    Structure

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import quote from './quote.js';
 
-import step1 from './travel/step1.js';
-import step2 from './travel/step2/step2.js';
-import step3 from './travel/step3/step3.js';
-import step4 from './travel/step4/step4.js';
+import step1 from './travel/single/step1.js';
+import step2 from './travel/single/step2/step2.js';
+import step3 from './travel/single/step3/step3.js';
+import step4 from './travel/single/step4/step4.js';
 
 const bupaGlobal = window.bupaGlobal || (window.bupaGlobal = {});
 
 Object.assign(bupaGlobal, {
   quote,
-  step1,
-  step2,
-  step3,
-  step4
+  travel: {
+    single: {
+      step1,
+      step2,
+      step3,
+      step4
+    }
+  }
 })
