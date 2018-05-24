3a2774e1b51246f406887c04496aceffc4e2df4c
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 20:27:17 +0100

    `single`

--- travel/single/index.js
+++ travel/single/index.js
@@ -1,9 +1,11 @@
+import single from './single.js';
 import step1 from './step1.js';
 import * as step2 from './step2/index.js';
 import * as step3 from './step3/index.js';
 import step4 from './step4/step4.js';
 
 export {
+  single,
   step1,
   step2,
   step3,
