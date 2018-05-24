e53a2370abfa9166096cdb79a13c5b3836239047
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 11:33:54 +0100

    RawGit

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-(function quote () {
-  const { location, location: { pathname } } = window;
-
-  if (location.toString().includes('bupaglobal')) {
-    Object
-      .keys(sessionStorage)
-      .forEach((key) => {
-        delete sessionStorage[key];
-      });
-
-    location.pathname = /^\/\w{2,3}\/quote/.test(pathname)
-      ? pathname.match(/(^\/\w{2,3}\/quote)/).shift()
-      : pathname.match(/(^\/\w{2,3})/).shift() + '/quote';
-  }
-})();
+import quote from './quote'
