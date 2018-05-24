4850cb53bffb3b8450034b371f5c2d973eed834c
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 19:57:14 +0100

    Debug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import quote from './quote.js';
 import single from './travel/single/index.js';
 import step1 from './travel/single/step1.js';
 import * as step2 from './travel/single/step2/index.js';
-import * as step3 from './travel/single/step3/step3.js';
+import * as step3 from './travel/single/step3/index.js';
 import step4 from './travel/single/step4/step4.js';
 import altapay from './travel/single/altapay.js';
 
