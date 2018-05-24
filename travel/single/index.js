5c1e48ba019d44671230d849e32d828e5e5f7528
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 22:15:14 +0100

    Refactor
    Chrome doesn’t like `export [etc] from [etc]`

--- travel/single/index.js
+++ travel/single/index.js
@@ -1,5 +1,13 @@
-export single from './single.js';
-export step1 from './step1.js';
-export * as step2 from './step2/index.js';
-export * as step3 from './step3/index.js';
-export step4 from './step4.js';
+import single from './single.js';
+import step1 from './step1.js';
+import * as step2 from './step2/index.js';
+import * as step3 from './step3/index.js';
+import step4 from './step4.js';
+
+export {
+  single,
+  step1,
+  step2,
+  step3,
+  step4
+};
