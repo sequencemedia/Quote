c683e06ce7cfdf276ace3a128d3df3fbb939b0dc
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-24 18:34:00 +0100

    `default`

--- travel/single/step2/index.js
+++ travel/single/step2/index.js
@@ -3,7 +3,7 @@ import selectBasic from './step2-select-basic.js';
 import selectBasicNonMedical from './step2-select-basic-non-medical.js';
 import selectBasicNonMedicalTripCancellation from './step2-select-basic-non-medical-trip-cancellation.js';
 
-export default {
+export {
   step2,
   selectBasic,
   selectBasicNonMedical,
