f6dfe3b3bb2827c1d648ae146dd3120b0e0ae90a
Jonathan Perry <jonathanperry@jonathanperry.com>
2018-05-27 23:18:20 +0100

    Refactor

--- travel/single/single.js
+++ travel/single/single.js
@@ -21,17 +21,29 @@ export default function single() {
 
     document.querySelectorAll('#country-quote-travel')
       .forEach((country) => {
+        country.dispatchEvent(new FocusEvent('focus', EVENT));
+        country.dispatchEvent(new MouseEvent('click', EVENT));
         country.value = 'United Kingdom';
         country.dispatchEvent(new Event('change', EVENT));
       });
 
     document.querySelectorAll('.alternative-country-select')
       .forEach((country) => {
+        country.dispatchEvent(new FocusEvent('focus', EVENT));
+        country.dispatchEvent(new MouseEvent('click', EVENT));
         country.selectedIndex = 228;
         country.dispatchEvent(new Event('change', EVENT));
       });
 
     const acceptPredictedCountry = document.querySelector('.accept-predicted-country');
     if (acceptPredictedCountry) acceptPredictedCountry.dispatchEvent(new MouseEvent('click', EVENT));
+  }
+
+  {
+    const button = document.querySelector('#step0-cta-continue');
+    if (button) {
+      button.dispatchEvent(new FocusEvent('focus', EVENT));
+      button.dispatchEvent(new MouseEvent('click', EVENT));
+    }
   }
 }
